54a55529b5695ff3255b44a073de82d66c2e8a46
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2026-09-04 17:13:58 +0200

    Enough for the return material page

--- Pages/ReturnMaterialPage.xaml.cs
+++ Pages/ReturnMaterialPage.xaml.cs
@@ -1,9 +1,12 @@
+using material_inout_desktop_v2.ViewModels;
+
 namespace material_inout_desktop_v2.Pages;
 
 public partial class ReturnMaterialPage : ContentPage
 {
-	public ReturnMaterialPage()
+	public ReturnMaterialPage(ReturnMaterialViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 }
