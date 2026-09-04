32aeb472fd273eae5c02fa1c50659a8c39431cdd
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2026-09-04 14:36:49 +0200

    Details view model bind parameter

--- ViewModels/VoucherDetailViewModel.cs
+++ ViewModels/VoucherDetailViewModel.cs
@@ -1,6 +1,26 @@
 ﻿namespace material_inout_desktop_v2.ViewModels;
 
-public class VoucherDetailViewModel : ViewModelBase
+public class VoucherDetailViewModel : ViewModelBase, IQueryAttributable
 {
+    public int VoucherID
+    {
+        get;
+        set
+        {
+            if (field != value)
+            {
+                field = value;
+                OnPropertyChanged(nameof(VoucherID));
+            }
+        }
+    }
 
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        string? voucherIDString = query["VoucherID"] as string;
+        if (!String.IsNullOrEmpty(voucherIDString) && int.TryParse(voucherIDString, out int vid))
+        {
+            VoucherID = vid;
+        }
+    }
 }
